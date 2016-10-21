1618783e7cf86cbc1d67f6ddd42acdafad6cbbcc
sfujiwara <shuhei.fujiwara@gmail.com>
2016-10-21 12:11:08 +0900

    modify to run on App Engine and Cloud ML

--- main.py
+++ main.py
@@ -1,41 +1,35 @@
-# restore trained data
-import tensorflow as tf
+# -*- coding: utf-8 -*-
+
+import logging
+
 import numpy as np
+from oauth2client.client import GoogleCredentials
+from googleapiclient import discovery
 
-import sys
-sys.path.append('mnist')
-import model
-
-x = tf.placeholder("float", [None, 784])
-sess = tf.Session()
-
-with tf.variable_scope("simple"):
-    y1, variables = model.simple(x)
-saver = tf.train.Saver(variables)
-saver.restore(sess, "mnist/data/simple.ckpt")
-def simple(input):
-    return sess.run(y1, feed_dict={x: input}).flatten().tolist()
-
-with tf.variable_scope("convolutional"):
-    keep_prob = tf.placeholder("float")
-    y2, variables = model.convolutional(x, keep_prob)
-saver = tf.train.Saver(variables)
-saver.restore(sess, "mnist/data/convolutional.ckpt")
-def convolutional(input):
-    return sess.run(y2, feed_dict={x: input, keep_prob: 1.0}).flatten().tolist()
+PROJECT_NAME = "cpb100demo1"
+MODEL_NAME = "mnist"
 
 # webapp
 from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/api/mnist', methods=['POST'])
 def mnist():
-    input = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0).reshape(1, 784)
-    output1 = simple(input)
-    output2 = convolutional(input)
+    input = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0).reshape(784)
+    credentials = GoogleCredentials.get_application_default()
+    ml = discovery.build("ml", "v1beta1", credentials=credentials)
+    data = {"instances": [{"image": input.tolist(), "key": 0}]}
+    req = ml.projects().predict(
+        body=data, name="projects/{0}/models/{1}".format(PROJECT_NAME, MODEL_NAME)
+    )
+    res = req.execute()
+    output1 = res["predictions"][0]["scores"]
+    output2 = [0] * 784
     return jsonify(results=[output1, output2])
 
-@app.route('/')
+
+@app.route('/', methods=["GET"])
 def main():
     return render_template('index.html')
