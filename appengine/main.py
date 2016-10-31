cbbdef1454956e82b6f15b3770f4410047384eab
sfujiwara <shuhei.fujiwara@gmail.com>
2016-10-31 15:38:06 +0900

    add CNN

--- appengine/main.py
+++ appengine/main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import json
 import logging
 
 import numpy as np
@@ -21,12 +22,17 @@ def mnist():
     credentials = GoogleCredentials.get_application_default()
     ml = discovery.build("ml", "v1beta1", credentials=credentials)
     data = {"instances": [{"image": input.tolist(), "key": 0}]}
-    req = ml.projects().predict(
+    req1 = ml.projects().predict(
+        body=data, name="projects/{0}/models/{1}/versions/{2}".format(PROJECT_NAME, MODEL_NAME, "softmax_regression")
+    )
+    req2 = ml.projects().predict(
         body=data, name="projects/{0}/models/{1}".format(PROJECT_NAME, MODEL_NAME)
     )
-    res = req.execute()
-    output1 = res["predictions"][0]["scores"]
-    output2 = [0] * 10
+    res1 = req1.execute()
+    res2 = req2.execute()
+    logging.info("response1: {}".format(json.dumps(res1)))
+    output1 = res1["predictions"][0]["scores"]
+    output2 = res2["predictions"][0]["scores"]
     return jsonify(results=[output1, output2])
 
 
